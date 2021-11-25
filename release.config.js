9d06d1526fb4d2bcf74eed876d54d5b07aed607c
Tholkkappiyan <tholki@icloud.com>
2021-11-25 13:30:04 +1100

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "branches": "master",
+    "repositoryUrl": "https://github.com/tholkkappiyan-x15/gheactions-cicd-workflow",
+    "plugins": [
+    "@semantic-release/commit-analyzer", 
+    "@semantic-release/release-notes-generator", 
+    "@semantic-release/github"]
+}
