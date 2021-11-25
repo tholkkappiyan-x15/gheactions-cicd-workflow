811d8af49858a4fa66257a0c15c892a59bc4df09
Tholkkappiyan <tholki@icloud.com>
2021-11-25 13:41:13 +1100

    feat: some feature

--- release.config.js
+++ release.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    "branches": "master",
-    "repositoryUrl": "https://github.com/tholkkappiyan-x15/gheactions-cicd-workflow",
-    "plugins": [
-    "@semantic-release/commit-analyzer", 
-    "@semantic-release/release-notes-generator", 
-    "@semantic-release/github"]
-}
+  branches: "master",
+  repositoryUrl:
+    "https://github.com/tholkkappiyan-x15/gheactions-cicd-workflow",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
